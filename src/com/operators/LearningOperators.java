33700fbd85abf726e648e95c409a457d2057080b
shiblee86 <syed.r.abdullah@gmail.com>
2019-03-12 16:25:09 -0400

    UdemyJava

---
+++ src/com/operators/LearningOperators.java
@@ -0,0 +1,29 @@
+package com.operators;
+
+public class LearningOperators {
+	
+	public static void main(String[] args) {
+		
+		int result = 1 + 2;
+		result = result + 5;
+		
+		System.out.println(result);
+		
+		result *= 10;
+		result++;
+		result += 2;
+		System.out.println(result);
+		
+		double d = (double) result%3;
+		
+		System.out.println(d);
+		
+boolean isAlien = false;
+
+if(isAlien == false) {
+	System.out.println("It not an Alien");
+}
+		
+	}
+
+}
