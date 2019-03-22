99ae9464ecbb96d92bba0e690e80a4565678d46b
Syed R Abdullah <syed.r.abdullah@gmail.com>
2019-03-21 21:58:56 -0400

    Challenges

--- src/codechallenges/TestSpeedConverter.java
+++ src/codechallenges/TestSpeedConverter.java
@@ -4,11 +4,11 @@ public class TestSpeedConverter {
 
     public static void main(String[] args) {
 
-        SpeedConverter.toMilesPerHour(70);
-
         long miles = SpeedConverter.toMilesPerHour(70);
 
         System.out.println(miles);
         //SpeedConverter.printConversion(70);
+
+        SpeedConverter.printConversion(70);
     }
 }
