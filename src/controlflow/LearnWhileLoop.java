4ea31677ec317f825fd9e644d1cc470a2a4c7763
Syed R Abdullah <syed.r.abdullah@gmail.com>
2019-04-17 22:37:35 -0400

    While and do while

---
+++ src/controlflow/LearnWhileLoop.java
@@ -0,0 +1,77 @@
+package controlflow;
+
+public class LearnWhileLoop {
+
+    public static void main(String[] args) {
+/*
+        int count = 0;
+        while (count != 5) {
+           System.out.println("Count Value is " + count);
+            count++;
+        }
+
+        count = 1;
+        //while(true) creates an infinite loop until we include a condition to break the loop.
+        while (true) {
+            if (count == 6) {
+                break;
+            }
+
+           System.out.println("The count is " + count);
+            count++;
+        }
+*/      int count = 6;
+        do{
+            System.out.println("Count value was " + count);
+            count++;
+            if(count > 100){
+                break;
+            }
+        }while (count != 6);
+        /*With do while loop, the code inside the block is executed first and then the condition
+        inside the while statement is valued.
+        For example, let's set the value of count to 7. So, Java will first first Count value was 6.
+        Once the print is complete, Java will then go inside while loop to check the condition. Java finds
+        the condition to be false since 7 is not equal to 6. Hence, it will not go back inside the do statement.
+         */
+
+        int number = 4;
+        int finishNumber = 20;
+        while(number <= finishNumber){
+            number++;
+
+            /*With continue, what is happening is, we are telling java,
+            if the number is not even, continue back to the while condition.
+             Do not go to the end of the loop (sout).
+             So, for example, for the first loop, when number = 4,
+             Java first evaluates the statement inside while.
+             Since 5 is less than the finishNumber,
+             Java increments number to take the value of 5.
+             Java then goes inside the if statement and evaluates.
+             Since 5 is not an even, java executes continue and goes back
+             to the code inside while.
+             When the number is 5,
+             Java increments number to 6
+             java evaluates the while statement,
+             Java then evaluates the if statement.
+             Since 6 is even, java executes the print statement and we have 6
+             as a printout.
+             */
+
+            //if(!isEvenNumber(number) -- It is another way of writing the line below
+            if(isEvenNumber(number) == false){
+                continue;
+            }
+            System.out.println("Event number is " + number);
+        }
+    }
+
+    public static boolean isEvenNumber(int number){
+        if(number % 2 != 0){
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
+
